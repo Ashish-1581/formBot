c46ffb6877690819539f095b5b88b67f4c8fc27e
Ashish-1581 <shahashish1581@gmail.com>
2024-07-31 22:31:49 +0530

    .

--- server/index.js
+++ server/index.js
@@ -12,17 +12,7 @@ const themeRoute=require("./routes/themeRoute");
 
 const app = express();
 app.use(cors());
-env.config(
-    {
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-        
-
-
-    }
-); 
+env.config(); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
